f0e57c1c92b0de033ce1d2779c8200fd4a023659
OGalloy <onedayaslion@gmail.com>
2023-05-14 20:36:15 +0500

    create inline keyboard

--- keyboard/keyboard.py
+++ keyboard/keyboard.py
@@ -4,18 +4,28 @@ from aiogram.types import InlineKeyboardMarkup
 from aiogram.types import InlineKeyboardButton
 
 
-#class constructor for inline keyboards
-class InlineKeyboard(InlineKeyboardMarkup):
-    def __init__(self, button_list: list):
-        self.keyboard = InlineKeyboardMarkup()
-        for button in button_list:
-            self.keyboard.add(button)
+class InlineButton(InlineKeyboardButton):
+    def __init__(self, button_text, callback_data):
+        super().__init__(text = button_text, callback_data = callback_data, parse_mode="HTML")
 
-#list of inline buttons
-buttons = [InlineKeyboardButton(text = "profile", callback_data = "button_profile"),
-            InlineKeyboardButton(text = "marathons", callback_data = "button_marathons"),
-            InlineKeyboardButton(text = "help", callback_data = "button_help")]
-markup = InlineKeyboard(buttons)
 
-#markup = InlineKeyboardMarkup()
-#markup.add(InlineKeyboardButton(text = "button_profile", callback_data = "button_profile_push"))
+
+#buttons
+profile_button = InlineButton(button_text = "profile", callback_data = "button_profile")
+marathons_button = InlineButton(button_text = "marathons", callback_data = "button_marathons")
+#help_button = InlineButton(button_text = "help", callback_data = "button_help")
+back_button = InlineButton(button_text = "back", callback_data = "button_back")
+
+#markups
+#main menu
+main_menu = InlineKeyboardMarkup()
+main_menu.add(profile_button)
+main_menu.add(marathons_button)
+
+#profile
+profile_menu = InlineKeyboardMarkup()
+profile_menu.add(back_button)
+
+#marathons
+marathons_menu = InlineKeyboardMarkup()
+marathons_menu.add(back_button)
--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
-from keyboard.keyboard import markup
+from keyboard.keyboard import main_menu, profile_menu, marathons_menu
 
 
 from config import TOKEN
 
 
-
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+#print(bot.get_chat())
+
+@dp.callback_query_handler(Text("button_profile"))
+async def button_profile_answer(callback_query: types.CallbackQuery):
+    await bot.delete_message(chat_id = callback_query["message"]["chat"]["id"], message_id = callback_query["message"]["message_id"])
+    await bot.send_message(chat_id = callback_query["from"]["id"], text= "profile", reply_markup=profile_menu)
+
+#lambda callback_query: "button_marathons"
+@dp.callback_query_handler(Text("button_marathons"))
+async def button_marathons_answer(callback_query: types.CallbackQuery):
+    await bot.delete_message(chat_id = callback_query["message"]["chat"]["id"], message_id = callback_query["message"]["message_id"])
+    await bot.send_message(chat_id = callback_query["from"]["id"], text= "marathons", reply_markup= marathons_menu)
+
+@dp.callback_query_handler(Text("button_back"))
+async def button_back_answer(callback_query: types.CallbackQuery):
+    await bot.delete_message(chat_id = callback_query["message"]["chat"]["id"], message_id = callback_query["message"]["message_id"])
+    await bot.send_message(chat_id = callback_query["from"]["id"], text= "menu", reply_markup=main_menu)
+
+
+
 @dp.message_handler(commands =["start"])
 async def cmd_start(message: types.Message):
-    await message.answer("MENU", reply_markup = markup.keyboard)
+    await message.answer("menu", reply_markup = main_menu)
 
-@dp.callback_query_handler(lambda callback_query: "button_profile_push")
-async def button_profile_answer(callback_query: types.CallbackQuery):
-    await callback_query.answer("you push \'profile\' button") 
+
 
 
 if __name__ == '__main__':
